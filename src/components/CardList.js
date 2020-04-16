4b8027faffe4c8fe5cd275b49e96094cdecfa93e
chaitali more <chaitali1266@gmail.com>
2020-04-16 12:39:46 +0530

    initial commit

---
+++ src/components/CardList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Card from './Card';
+const CardList = ({robots}) => {
+        // const cardArray = robots.map((user,i) =>{
+        //     return(
+        //      <Card 
+        //     key={robots[i].id} 
+        //     id={robots[i].id} 
+        //     name={robots[i].name} 
+        //     email={robots[i].email}
+        //     />
+        //     );
+        // })
+        if(true){
+            throw new Error('NOOOOOOO!');
+        }
+    return(
+        <div>
+            {/* {cardArray} */}
+            {
+                robots.map((user,i) =>{
+                return(
+                <Card 
+                key={robots[i].id} 
+                id={robots[i].id} 
+                name={robots[i].name} 
+                email={robots[i].email}
+                />
+                );
+                })
+             }   
+        </div>
+    );    
+    
+}
+
+export default CardList;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
